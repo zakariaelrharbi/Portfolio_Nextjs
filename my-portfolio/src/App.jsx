a3276818ade698fa184cc036997025cd0fc14fe0
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-09 23:27:09 +0100

    [tailwind + react] : instalation done

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
+     <h1 className="text-5xl font-bold underline text-orange-500">
       Hello world!
     </h1>
       <div>
