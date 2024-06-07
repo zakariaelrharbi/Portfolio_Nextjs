cf21508fefe62c46cbaf492f7869a660ec74fb02
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-06-07 12:32:38 +0100

    adding font and color

--- my-portfolio/app/layout.jsx
+++ my-portfolio/app/layout.jsx
@@ -1,7 +1,10 @@
-import { Inter } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const JetBrainsMono = JetBrains_Mono({ subsets: ["latin"],
+  weights: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-jetbrainsmono",
+ });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={JetBrainsMono.className}>{children}</body>
     </html>
   );
 }
--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -1,3 +1,5 @@
+const { DEFAULT_RUNTIME_WEBPACK } = require('next/dist/shared/lib/constants');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
@@ -11,12 +13,28 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
+      padding: "15px",
       },
-    },
+    screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '960px',
+        xl: '1200px',
+      },
+      fontFamily: {
+        primary: "var(--font-jetbrainsmono)",
+      },
     extend: {
+      colors: {
+        primary: "#1c1c22",
+        accent:{
+          DEFAULT: "#00ff99",
+          hover: "#00e187",
+        },
+        
+      },
+
+
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
