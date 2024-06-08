bd3daa7e191bdf84670220b38f11bb1dbc902c99
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-06-08 10:43:12 +0100

    styling nav bar done

--- my-portfolio/components/Header.jsx
+++ my-portfolio/components/Header.jsx
@@ -7,21 +7,29 @@ import Nav from "./Nav";
 
 
 const Header = () => {
-  return <header className="py-8 lg:py-12 bg-pink-50/20 text-white ">
+  return <header className="py-8 lg:py-12  text-white ">
     <div className="container max-auto flex justify-between items-center">
         {/* logo */}
         <Link href="/">
-            <h1>
+            <h1 className="text-4xl font-semibold">
                 <span className="text-3xl font-semibold">Zer</span>
-                Dev
-                <span className="text-accent">.</span>
+                Dev<span className="text-accent">.</span>
             </h1>
         </Link>
 
-        {/* Desktop Nav */}
+        {/* Desktop Nav  & hire me*/}
         {/* hide it in the mobile size */}
-        <div className="hidden xl:flex">
+        <div className="hidden xl:flex items-center gap-8">
             <Nav />
+            <Link href="/contact">
+                <Button className="ml-4">Hire Me</Button>
+            </Link>
+        </div>
+
+        {/* Mobile Nav */}
+        {/* hide it in the desktop size */}
+        <div className="xl:hidden flex items-center ">
+            mobile
         </div>
 
     </div>
