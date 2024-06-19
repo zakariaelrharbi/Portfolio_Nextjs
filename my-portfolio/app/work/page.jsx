b64235f7a07f30c7eac6a663b2fc0bb30b7daf8b
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-06-19 15:58:28 +0100

    my work section

--- my-portfolio/app/work/page.jsx
+++ my-portfolio/app/work/page.jsx
@@ -1,3 +1,15 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import React, {useState} from 'react';
+
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Work = () => {
   return (
