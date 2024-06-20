4e311cb9c2d1074b08ffc27b85c5f05b6dec1700
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-06-20 16:38:56 +0100

    adding work

--- my-portfolio/app/work/page.jsx
+++ my-portfolio/app/work/page.jsx
@@ -158,7 +158,7 @@ const Work = () => {
                   <SwiperSlide key={index} className='w-full'>
                     <div className='h-[460px] relative group flex justify-center items-center bg-pink-50/20'>
                     {/* overlay */}
-                    <div>
+                    <div className='absolute top-0 bottom-0 w-full h-full bg-black/10 z-10'>
 
                     </div>
                     {/* images */}
@@ -169,6 +169,7 @@ const Work = () => {
                   </SwiperSlide>
                 )
               })}
+              
             </Swiper>
           </div>
         </div>
