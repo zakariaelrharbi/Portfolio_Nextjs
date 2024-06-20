8203f01daf0cdf425dad8f401bd4e115822485cb
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-06-20 17:28:38 +0100

    adding conatct

--- my-portfolio/app/contact/page.jsx
+++ my-portfolio/app/contact/page.jsx
@@ -1,4 +1,33 @@
-import React from 'react'
+'use client'
+
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger } from "@radix-ui/react-select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: 'Phone',
+    description: '(+212) 6 82 88 30 65'
+  },
+  {
+    icon: <FaEnvelope />,
+    title: 'Email',
+    description: 'zakariaelrharbi269@gmail.com'
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: 'Address',
+    description: 'Casablanca, Morocco'
+  }
+]
+
+
 
 const Contact = () => {
   return (
