fa78d1bebaaf5dd83afc97c298ec18d803c3df0a
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-12-03 22:52:50 +0100

    contact completed

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -36,7 +36,7 @@ const Contact = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
-          <div className="xl:h-[54%] order-2 xl:order-none w-full">
+          <div className="xl:h-[50%] order-2 xl:order-none w-full">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent">Let's work together</h3>
               <p className="text-white/70">
