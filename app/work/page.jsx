976ea5b99ef23bd594f20d0084304fd0184c4ba2
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-12-24 15:30:47 +0100

    work image mobile size fixed

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -184,14 +184,14 @@ const Work = () => {
               {projects.map((project, index) => {
                 return (
                   <SwiperSlide key={index} className='w-full'>
-                    <div className='h-[300px] xl:h-[460px] relative group flex justify-center items-center bg-pink-50/20'>
+                    <div className='h-[34vh] xl:h-[460px] relative group flex justify-center items-center bg-pink-50/20'>
                     {/* overlay */}
                     <div className='absolute top-0 bottom-0 w-full h-full bg-black/10 z-10'>
 
                     </div>
                     {/* images */}
                     <div className='relative w-full h-full'> 
-                      <Image src={project.image} fill  className='object-contain ' alt=''/>
+                      <Image src={project.image} fill  className='object-cover ' alt=''/>
                     </div>
                     </div>
                   </SwiperSlide>
